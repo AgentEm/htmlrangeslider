90d2f7d23e874a3e78e7f4423926e3738958dd1f
agentem <eeporta@gmail.com>
2014-09-22 10:38:22 -0400

    initial commit

---
+++ range.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	var $slider = $('.slider');
+
+	$slider.on("change click", function(event){
+		$('.slider').hide().show(0);
+		if (event.which === 1){
+			var maxValue = $('.slider').prop('max');
+			var $this = $(this);
+			var rangeWidth = ($this.val() / maxValue * 100) + "%";
+			$('.range-fill').css("width", rangeWidth);
+		}
+	});
+
+	$slider.on("mouseup click", function() {
+			var $this = $(this);
+			$(this).val(parseInt(Math.round($this.val())));
+	});
+});
+
+//mousedown is no b/c you need to click down and off before it renders
+//mouseup is no b/c nothing changes until you unclick
+//change is no because it breaks it
+//mousemove
