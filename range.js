75aebe2131f21f6cfbf6fcebf6dc3274ff9c6612
agentem <eeporta@gmail.com>
2014-09-22 22:26:21 -0400

    this seems to work for mobile safari?

--- range.js
+++ range.js
@@ -1,25 +1,20 @@
 $(document).ready(function(){
 	
-	var $slider = $('.slider');
+	var $slider = $('input[type=range]');
 
-	$slider.on("change click", function(event){
-		event.preventDefault();
-		$('.slider').hide().show(0);
-		if (event.which === 1){
-			var maxValue = $('.slider').prop('max');
-			var $this = $(this);
-			var rangeWidth = ($this.val() / maxValue * 100) + "%";
-			$('.range-fill').css("width", rangeWidth);
-		}
+	$slider.on("mousedown click", function() {
+	    var $this = $(this);
+	    $(this).val(parseInt(Math.round($this.val())));
 	});
 
-	$slider.on("mouseup click", function() {
-			var $this = $(this);
-			$(this).val(parseInt(Math.round($this.val())));
+	$slider.on("touchend mousemove click", function(event){
+	  $slider.hide().show(0);
+        console.log(event);
+	  if (event.which === 1 || event.which === 0){
+	    var maxValue = $slider.prop('max');
+	    var $this = $(this);
+	    var rangeWidth = ($this.val() / maxValue * 100) + "%";
+	    $('.range-fill').css("width", rangeWidth);
+	  }
 	});
 });
-
-//mousedown is no b/c you need to click down and off before it renders
-//mouseup is no b/c nothing changes until you unclick BUT this is also the closest to what we want
-//change is no because it breaks it
-//mousemove is no becuase the range-fill will just go wherever I put my mouse last
