56cd91f37c1fd69a694beb4dbf955377321d1c56
agentem <eeporta@gmail.com>
2014-09-23 11:27:36 -0400

    akfds

--- range.js
+++ range.js
@@ -2,23 +2,23 @@ $(document).ready(function(){
 	
 	var $slider = $('input[type=range]');
  
+    function convertTouchXToFraction(touchX) {
+        //Figure out where it will be on the slider
+        var sliderOffset = $slider.offset();
+        var normalizedX = touchX - sliderOffset.left;
+        return normalizedX / $slider.width();
+    }
 
-    // Mnap to closest value
-	$slider.on("touchend mousedown click", function(event) {
-	    var $this = $(this);
-        var inputValue = parseInt(Math.round($this.val()));
+    var stepCount = parseInt($slider.attr('max')) + 1;
+    // Snap to closest value
+	$slider.on("touchend mousedown click", function snap(event) {
         // Get the max
         if(event.type === 'touchend') {
-            var max = parseInt($slider.attr('max'));
             //Get the touch location
             var touchX = event.originalEvent.changedTouches[0].clientX;
-            //Figure out where it will be on the slider
-            var sliderOffset = $slider.offset();
-            touchX = touchX - sliderOffset.left;
-           
-            var intervals = Math.round($slider.width() / (max + 1));
-            var value = Math.floor(touchX / intervals);
-            inputValue = value;
+            var frac = convertTouchXToFraction(touchX);
+
+            inputValue = Math.round(frac * stepCount);
         }
        
     	$(this).val(inputValue);
@@ -37,13 +37,12 @@ $(document).ready(function(){
     // Modify range-fill as ball move.
 	$slider.on("touchmove mousemove click", function(event){
         console.log(event);
-        console.log("val is", $(this).val());
-      $slider.hide().show(0);
-	  if (event.which === 1 || event.which === 0){
-	    var maxValue = $slider.attr('max');
-	    var $this = $(this);
-	    var rangeWidth = ($this.val() / maxValue * 100) + "%";
-	    $('.range-fill').css("width", rangeWidth);
-	  }
+        $slider.hide().show(0);
+	    if (event.which === 1 || event.which === 0){
+            var touchX = event.originalEvent.changedTouches[0].clientX;
+            var frac = convertTouchXToFraction(touchX);
+	        var rangeWidth = (frac * 100) + "%";
+	        $('.range-fill').css("width", rangeWidth);
+	    }
 	});
 });
