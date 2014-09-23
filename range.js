68c46f144b031c0796ab0a1d7431ed8f5b5ae4eb
agentem <eeporta@gmail.com>
2014-09-23 10:26:29 -0400

    trying other guy's jsfiddle

--- range.js
+++ range.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
 	
 	var $slider = $('input[type=range]');
-
-	$slider.on("mousedown click", function() {
+ 
+	$slider.on("touchend mousedown click", function(event) {
 	    var $this = $(this);
-	    $(this).val(parseInt(Math.round($this.val())));
+        var inputValue = parseInt(Math.round($this.val()));
+        // Get the max
+        if(event.type === 'touchend') {
+            var max = parseInt($slider.attr('max'));
+            //Get the touch location
+            var touchX = event.originalEvent.changedTouches[0].clientX;
+            //Figure out where it will be on the slider
+            var sliderOffset = $slider.offset();
+            touchX = touchX - sliderOffset.left;
+            //intervals
+            //console.log(touchX);
+           
+            var intervals = Math.round($slider.width() / (max + 1));
+            var value = Math.floor(touchX / intervals);
+            inputValue = value;
+        }
+       
+    	$(this).val(inputValue);
+        
 	});
 
-	$slider.on("mousemove click", function(event){
+	$slider.on("touchend mousemove click", function(event){
 	  $slider.hide().show(0);
-	  if (event.which === 1){
+	  if (event.which === 1 || event.which === 0){
 	    var maxValue = $slider.prop('max');
 	    var $this = $(this);
 	    var rangeWidth = ($this.val() / maxValue * 100) + "%";
