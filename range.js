e7634d2964c36d86f606facb6a9391e73603c909
agentem <eeporta@gmail.com>
2014-09-22 22:52:44 -0400

    mc

--- range.js
+++ range.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	
 	var $slider = $('input[type=range]');
 
-	$slider.on("touchend mousedown click", function() {
+	$slider.on("mousedown click", function() {
 	    var $this = $(this);
 	    $(this).val(parseInt(Math.round($this.val())));
 	});
 
-	$slider.on("touchend mousemove click", function(event){
+	$slider.on("mousemove click", function(event){
 	  $slider.hide().show(0);
 	  if (event.which === 1){
 	    var maxValue = $slider.prop('max');
