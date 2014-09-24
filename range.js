0dedfa92bdfc9de9111be84019c7a2549fc8de5f
agentem <eeporta@gmail.com>
2014-09-24 13:03:11 -0400

    changed fill div styling and positioning so that it's under the ball

--- range.js
+++ range.js
@@ -13,12 +13,12 @@ $(document).ready(function(){
 
     function changeHandler(event) {
         var value = $slider.val();
-        console.log("change handled", value);
+
         if (lastChangeHandled !== value) {
             lastChangeHandled = value;
             var frac = value / max;
             var rangeWidth = (frac * 100) + "%";
-            $('.range-fill').css("width", rangeWidth);
+            $('.range-fill').css({"width": rangeWidth, "min-width": "5%", "max-width": "98%"});
             $slider.hide().show(0);
         }
     }
@@ -39,11 +39,9 @@ $(document).ready(function(){
             // This is required because on iPad you get:
             //  touchEnd -> 300ms delay -> mouseMove
             var throttleMs = 400;
-            console.log("timeDiff", timeDiff)
             if (timeDiff > throttleMs &&
                   (event.which === 1 || touchRegexp.test(event.type))) {
 
-                console.log("move event", event)
                 var touchX = event.originalEvent.clientX || event.originalEvent.changedTouches[0].clientX;
                 var frac = convertTouchXToFraction(touchX);
                 var inputValue;
@@ -53,7 +51,7 @@ $(document).ready(function(){
                     inputValue = Math.round(inputValue);
                     setLastSnap();
                 }
-                // console.log("syncing val", inputValue)
+
                 $slider.val(inputValue);
                 // jQuery does not trigger change for non-user interactions.
                 changeHandler(event);
